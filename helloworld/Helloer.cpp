3a86820b900df58b351e2462cace1b491542a137
RusProgrammer <ilkaboomboom@rambler.ru>
2016-11-12 16:07:50 +0300

    Check kommit

---
+++ helloworld/Helloer.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Helloer.cpp
+ * Author: fedosov
+ * 
+ * Created on November 12, 2016, 4:06 PM
+ */
+
+#include "Helloer.h"
+
+Helloer::Helloer() {
+}
+
+Helloer::Helloer(const Helloer& orig) {
+}
+
+Helloer::~Helloer() {
+}
+
---
+++ helloworld/Helloer.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Helloer.h
+ * Author: fedosov
+ *
+ * Created on November 12, 2016, 4:06 PM
+ */
+
+#ifndef HELLOER_H
+#define HELLOER_H
+
+class Helloer {
+public:
+    Helloer();
+    Helloer(const Helloer& orig);
+    virtual ~Helloer();
+private:
+
+};
+
+#endif /* HELLOER_H */
+
